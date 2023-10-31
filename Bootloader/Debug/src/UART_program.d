af1b7cb058b275360b9aa0327410d75975e3ca05
Hosam Ayoub <hosamayoub50@gmail.com>
2023-10-31 13:25:42 +0200

    new bootloader with checksum and odd records

---
+++ Bootloader/Debug/src/UART_program.d
@@ -0,0 +1,8 @@
+src/UART_program.o: ../src/UART_program.c ../include/LIB/STD_TYPES.h \
+ ../include/LIB/BIT_MATH.h ../include/MCAL/UART/UART_interface.h \
+ ../include/MCAL/UART/UART_private.h ../include/MCAL/UART/UART_config.h
+../include/LIB/STD_TYPES.h:
+../include/LIB/BIT_MATH.h:
+../include/MCAL/UART/UART_interface.h:
+../include/MCAL/UART/UART_private.h:
+../include/MCAL/UART/UART_config.h:
---
+++ Bootloader/Debug/src/main.d
@@ -0,0 +1,14 @@
+src/main.o: ../src/main.c ../include/LIB/STD_TYPES.h \
+ ../include/LIB/BIT_MATH.h ../include/MCAL/RCC/RCC_interface.h \
+ ../include/MCAL/STK/STK_interface.h ../include/MCAL/FMI/FMI_interface.h \
+ ../include/MCAL/GPIO/GPIO_interface.h \
+ ../include/MCAL/UART/UART_interface.h \
+ ../include/Service/HexParser/HexParser_interface.h
+../include/LIB/STD_TYPES.h:
+../include/LIB/BIT_MATH.h:
+../include/MCAL/RCC/RCC_interface.h:
+../include/MCAL/STK/STK_interface.h:
+../include/MCAL/FMI/FMI_interface.h:
+../include/MCAL/GPIO/GPIO_interface.h:
+../include/MCAL/UART/UART_interface.h:
+../include/Service/HexParser/HexParser_interface.h:
